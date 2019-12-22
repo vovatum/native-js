13335d792a1841d7473ce5a801b6c1febf01de4f
vovkahimik <vovkahimik@gmail.com>
2019-12-23 00:01:02 +0300

    lesson/array tasks 3-4

---
+++ lesson/test.js
@@ -0,0 +1,5 @@
+let friendsNames = ['Andrey', 'Vasja', 'Kirill'];
+let friendsAges = [23, 30, 25];
+let friends = [];
+friends = friendsNames.map(item, index => item = {index});
+console.log(friends);
