82776612d68ab4db50dad734b6b1f4f9499e4ff1
vovkahimik <vovkahimik@gmail.com>
2020-03-02 13:20:55 +0300

    Native incubator reduce name

--- lesson/test.js
+++ lesson/test.js
@@ -1,5 +1,4 @@
-let friendsNames = ['Andrey', 'Vasja', 'Kirill'];
-let friendsAges = [23, 30, 25];
-let friends = [];
-friends = friendsNames.map(item, index => item = {index});
-console.log(friends);
+let myNames = ['Andrey', 'Vasja', 'Kirill'];
+let persons = [];
+persons = myNames.map(name => name = {name});
+console.log(persons);
