565b4b5c068c8ae51ad96ad18234ccc8f79c6719
vovkahimik <vovkahimik@gmail.com>
2020-03-18 20:21:29 +0300

    Training JS #20: Methods of String object--charAt() charCodeAt() and fromCharCode()

--- codewars/Training JS #20 Methods of String object--charAt() charCodeAt() and fromCharCode().js
+++ codewars/Training JS #20 Methods of String object--charAt() charCodeAt() and fromCharCode().js
@@ -2,7 +2,9 @@ function topSecret(str) {
     let words = str.split(" ");
     let chars = words
         .map(e => e.split("")
-            .map(e => String.fromCharCode(e.charCodeAt() - 3)))
+            .map(e => 90 < e.codePointAt(0) < 97 //условие выполняется и изменяет всю строку
+                ? String.fromCharCode(e.charCodeAt() - 7)
+                : String.fromCharCode(e.charCodeAt() - 3)))
         .map(e => e.join(""))
         .join(" ");
     return chars;
@@ -13,3 +15,18 @@ console.log(topSecret("wklv lv dq hadpsoh"));
 console.log(topSecret("Khoor Zruog!"));
 console.log("A".codePointAt(0));
 console.log("z".codePointAt(0));
+console.log("a".codePointAt(0));
+
+// function topSecret(str) {
+//     let words = str.split(" ");
+//     let chars = words
+//         .map(e => e.split("")
+//             .map(e => String.fromCharCode(e.charCodeAt() - 3)))
+//         .map(e => e.join(""))
+//         .join(" ");
+//     return chars;
+// }
+// решение без учёта символов:
+// M_ name is John
+// this is an e^ample
+// Hello World
