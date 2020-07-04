02247e9eb766df047374360431db35fe3d853ca2
vovkahimik <vovkahimik@gmail.com>
2020-07-04 21:35:15 +0300

    попытка избавиться

---
+++ codewars/Training JS #31 methods of arrayObject---isArray() indexOf() and toString().js
@@ -0,0 +1,23 @@
+function blackAndWhite(arr) {
+    if (!Array.isArray(arr))
+        return "It's a fake array"
+    let arrOne = arr.filter(num => {
+        if (num === 5) {
+            return num
+        }
+    })
+    let arrTwo = arr.filter(num => {
+        if (num === 13) return num
+    })
+    if (arrOne.length && arrTwo.length) {
+        return "It's a black array"
+    } else
+        return "It's a white array"
+}
+
+console.log(blackAndWhite(5, 13))
+// "It's a fake array");
+console.log(blackAndWhite([5, 13]))
+// , "It's a black array");
+console.log(blackAndWhite([5, 12]))
+// , "It's a white array");
